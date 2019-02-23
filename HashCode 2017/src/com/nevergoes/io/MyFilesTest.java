bcf42b9d8717dcae2cdfef3da1225cac3ce2dacc
Alejandro López <aloro2392@gmail.com>
2019-02-23 12:31:05 +0100

    Write file changes

--- HashCode 2017/src/com/nevergoes/io/MyFilesTest.java
+++ HashCode 2017/src/com/nevergoes/io/MyFilesTest.java
@@ -1,5 +1,7 @@
 package com.nevergoes.io;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import com.nevergoes.assertions.MyAsserts;
@@ -14,8 +16,14 @@ public class MyFilesTest {
 
 		System.out.println("Running test: " + MyFilesTest.class.getSimpleName());
 
-		List<String> lines = MyFiles.read("testFile.txt");
+		// Test readFile
+		List<String> lines = MyFiles.readFile("testFile.txt");
 		MyAsserts.isTrue(lines.size() == 3, "Fileread test failed!");
+
+		// Test writeFile
+		MyFiles.writeFile("test.txt", "Testing");
+		Path path = Paths.get("resources/test.txt");
+		MyAsserts.isTrue(path.toFile().isFile(), "File writing test failed!");
 
 		return true;
 	}
